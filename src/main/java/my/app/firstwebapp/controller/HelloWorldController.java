38e5899af1693b595d73ab391b5c29bb702d7fad
Archie7054 <13405955051@139.com>
2020-11-30 18:20:36 +0800

    program two version 1201

--- src/main/java/my/app/firstwebapp/controller/HelloWorldController.java
+++ src/main/java/my/app/firstwebapp/controller/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/one")
+@RequestMapping("/two")
 public class HelloWorldController {
     @RequestMapping("/sayhello")
     public String say(){
