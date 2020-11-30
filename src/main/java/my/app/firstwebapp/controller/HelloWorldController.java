419078df0b3e030298b6afc4c73de7b776ae2471
Archie7054 <13405955051@139.com>
2020-11-30 11:36:25 +0800

    Version 1105

--- src/main/java/my/app/firstwebapp/controller/HelloWorldController.java
+++ src/main/java/my/app/firstwebapp/controller/HelloWorldController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/one")
 public class HelloWorldController {
-    @RequestMapping("/say")
+    @RequestMapping("/sayhello")
     public String say(){
-        return "Hello,This is Version 1104";
+        return "Hello,This is Version 1101";
     }
 }
